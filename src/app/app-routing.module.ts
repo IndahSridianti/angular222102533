e9b690536ce560fbd59fe7ec1a4e5432d6fdbab1
IndahSridianti <indahsridianti9@gmail.com>
2023-10-18 07:25:18 +0700

    tugas 7 update dashboard v3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { Dashboard3Component } from './dashboard3/dashboard3.component';
 
 const routes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'dashboard2',
+    component: Dashboard2Component,
+  },
+  {
+    path: 'dashboard3',
+    component: Dashboard3Component,
   },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
