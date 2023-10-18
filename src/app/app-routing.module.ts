51d65253df3ae7182375bb2ff76b488e686ede4d
IndahSridianti <indahsridianti9@gmail.com>
2023-10-18 23:07:31 +0700

    tugas 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,22 +8,10 @@ import { Dashboard2Component } from './dashboard2/dashboard2.component';
 import { Dashboard3Component } from './dashboard3/dashboard3.component';
 
 const routes: Routes = [
-  {
-    path: 'admin',
-    component: AdminComponent,
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent,
-  },
-  {
-    path: 'dashboard2',
-    component: Dashboard2Component,
-  },
-  {
-    path: 'dashboard3',
-    component: Dashboard3Component,
-  },
+  { path: 'admin', component: AdminComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard2', component: Dashboard2Component },
+  { path: 'dashboard3', component: Dashboard3Component },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
