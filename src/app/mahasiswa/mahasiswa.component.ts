b0c10baa78dba5567e6ebb6a0bc9834741d4d28b
IndahSridianti <indahsridianti9@gmail.com>
2023-11-22 00:26:08 +0700

    sidebar mahasiswa

---
+++ src/app/mahasiswa/mahasiswa.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-mahasiswa',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './mahasiswa.component.html',
+  styleUrl: './mahasiswa.component.css'
+})
+export class MahasiswaComponent {
+
+}
