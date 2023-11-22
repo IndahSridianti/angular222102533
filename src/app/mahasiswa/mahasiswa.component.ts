8ab146c396f6d670baaa4dd738d735384394dd1c
IndahSridianti <indahsridianti9@gmail.com>
2023-11-22 11:07:50 +0700

    api done

--- src/app/mahasiswa/mahasiswa.component.ts
+++ src/app/mahasiswa/mahasiswa.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+declare const $ : any;
+
 
 @Component({
   selector: 'app-mahasiswa',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './mahasiswa.component.html',
   styleUrl: './mahasiswa.component.css'
 })
-export class MahasiswaComponent {
+export class MahasiswaComponent implements OnInit,AfterViewInit {
 
+  data:any;
+  table1:any;
+  
+  constructor (private http: HttpClient){
+
+  }
+  ngAfterViewInit(): void {
+    this.table1 = $("#table1").DataTable();
+    this.bind_mahasiswa();
+  }
+  ngOnInit(): void {
+    
+  }
+  // 
+  bind_mahasiswa(): void {
+    this.http.get("https://stmikpontianak.net/011100862/tampilMahasiswa.php")
+      .subscribe((data: any) => {
+        console.log(data);
+        
+        data.forEach((e: any) => {
+          let ttl = e.TempatLahir + ", " + e.TanggalLahir;
+
+          let rows = [
+            e.NIM,
+            e.Nama,
+            e.JenisKelamin,
+            ttl,
+            e.JP,
+            e.Alamat,
+            e.StatusNikah,
+            e.TahunMasuk
+          ];
+
+          this.table1.row.add(rows);  // Menambahkan baris ke DataTable
+        });
+
+        this.table1.draw(false);  // Menggambar tabel setelah menambahkan semua baris
+      });
+  }
 }
